7e9792eff3c1dce24c0394783c5a12e713985be9
Ankita Yadav <ankita@Administrators-MacBook-Air.local>
2023-08-14 16:23:40 +0530

    Movie

---
+++ src/main/java/dev/ankita/movies/MoviesApplication.java
@@ -0,0 +1,17 @@
+package dev.ankita.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+//@EnableAutoConfiguration(basePackageClasses = MovieRepository.class)
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);}
+
+}
